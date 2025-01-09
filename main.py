8a466c52e1dcdc76eee576180074b0990a23ea3a
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2025-01-09 16:55:28 +0000

    Created the paddles and movement on both

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen
+from paddle import Paddle
 
 WIDTH = 800
 HEIGHT = 600
@@ -7,9 +8,21 @@ screen = Screen()
 screen.setup(width= WIDTH, height=HEIGHT)
 screen.bgcolor("black")
 screen.title("Pong Game")
+screen.tracer(0)
 
+paddle1 = Paddle(365)
+paddle2 = Paddle(-370)
 
+screen.listen()
+screen.onkey(paddle1.move_up, "Up")
+screen.onkey(paddle1.move_down, "Down")
 
+screen.onkey(paddle2.move_up, "w")
+screen.onkey(paddle2.move_down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 
 screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+
+WIDTH = 5
+LENGTH = 1
+
+class Paddle(Turtle):
+    def __init__(self,position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=WIDTH, stretch_len=LENGTH)
+        self.penup()
+        self.goto(position, 0)
+
+    def move_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def move_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
